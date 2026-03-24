3b17d8b0f632829fd87565598e545f3164cbafb8
Mayank Gupta <mayankguptag055@gmail.com>
2026-03-24 22:54:48 +0530

    Create 10003-lyft-driver-wages.py

---
+++ 10003-lyft-driver-wages.py
@@ -0,0 +1,5 @@
+https://platform.stratascratch.com/coding/10003-lyft-driver-wages?code_type=2
+
+import pandas as pd
+import numpy as np
+result = lyft_drivers[(lyft_drivers['yearly_salary'] <= 30000) | (lyft_drivers['yearly_salary'] >= 70000)]
